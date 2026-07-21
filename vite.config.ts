ef50a4b680c834b3503b76d8597dc5ac295216fe
gamecashapp <95624425+gamecashapp@users.noreply.github.com>
2026-07-21 15:24:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './ghostwriting-s/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
